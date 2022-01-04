2e5cf55027dc613e40f25ff05a7696497cbabbd1
Vacha Shah <vachshah@amazon.com>
2022-01-03 18:06:48 -0800

    Testing auto backport
    Signed-off-by: Vacha Shah <vachshah@amazon.com>

--- src/main/java/SampleJava.java
+++ src/main/java/SampleJava.java
@@ -4,6 +4,6 @@
 public class SampleJava {
 
     public static void main (String[] args) {
-	    System.out.println ("Sample Java file to test");
+	    System.out.println ("Sample file to test");
     }
 }
