a70af916c20ec04a50b4d2ef9b6f8f3d668b194d
VachaShah <VachaShah@users.noreply.github.com>
2022-01-31 23:07:20 +0000

    Committing changes
    Signed-off-by: Vacha <vachshah@amazon.com>

--- src/main/java/SampleJava.java
+++ src/main/java/SampleJava.java
@@ -6,5 +6,6 @@ public class SampleJava {
     public static void main (String[] args) {
 	    System.out.println ("Sample file to test");
         System.out.println ("Adding a new line");
+    System.out.println ("Adding a new line from github action");
     }
 }
