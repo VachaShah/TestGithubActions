54ed0937089308b82443b4f1d95d3b3113eedcc8
Vacha Shah <vachshah@amazon.com>
2022-05-12 16:49:17 -0700

    Testing backport with new file (#57)
    Signed-off-by: Vacha Shah <vachshah@amazon.com>

---
+++ src/main/java/SampleJavaCopy.java
@@ -0,0 +1,11 @@
+/**
+* Sample java file.
+*/
+public class SampleJavaCopy {
+
+    public static void main (String[] args) {
+	    System.out.println ("Sample file to test");
+        System.out.println ("Adding a new line");
+    System.out.println ("Adding a new line from github action");
+    }
+}
